e4750432ae71ac778a80d470ebb1c6a89e8316a2
emaniprateek <prateek3979@gmail.com>
2023-11-04 10:51:43 +0530

    Siddarth

--- script.js
+++ script.js
@@ -135,6 +135,7 @@ function fruitProcessor(apples, oranges){
 console.log(fruitProcessor(2,3));
 */
 
+/*
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
@@ -147,3 +148,248 @@ const yearsUntilRetirement = function (birthyYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1970, 'Mike'));
+*/
+/*
+const friend1 = 'Prateek'
+const friend2 = 'Harshith'
+const friend3 = 'Sukeerth'
+
+const friends = ['Prateek','Harshith','Sukeerth'];
+console.log(friends);
+
+const years = new Array(2001,2000,1999,2001);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+friends[2]= 'Pavan';
+console.log(friends);
+
+*/
+/*
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
+console.log(age1, age2, age3);  */
+/*
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037-1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age:2037-99,
+    job:'teacher',
+    friends:['Michael','Peter','Steven']
+};
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+const interestedIn = prompt('What do you want to know about jonas? Choose between firstName,lastName,age,job, and friends');
+console.log(jonas[interestedIn]);
+*/
+/*
+const jonas2 = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear:1991,
+    job: 'teacher',
+    friends: ['Michael','Peter','Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function () {
+        console.log(this);
+        return 2037 - this.birthYear;
+    }
+};
+
+console.log(jonas2.calcAge());
+console.log(jonas2.calcAge());
+console.log(jonas2.calcAge());
+console.log(jonas2.calcAge());
+*/
+/*
+for (let rep = 1; rep <= 10; rep++) {
+    console.log('Lifting weights repetition ${rep}');
+}
+*/
+/*
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i]);
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages); */
+
+/*const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+     ['Michael', 'Peter', 'Steven'],
+     true
+];
+
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i]);
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+*/
+/*
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+*/
+/*
+'use strict';
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
+*/
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {};
+*/
+/*
+const calcTempAmplitude = function (temps) {
+    let max = temps[0];
+    let min = temps[0];
+    for (let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i];
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+
+    }
+    console.log(max, min);
+    return max - min;
+};
+const amplitudeNew = calc
+*/
+/*
+
+'use strict';
+
+console.log(document.querySelector('.message'),
+textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+
+document.querySelector('.guess').value = 23;
+
+console.log(document.querySelector('.guess').value);
+*/
+
+/*
+const number = Math.random();
+
+document.querySelector('.check').addEventListener ('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+console.log(guess, typeof guess);
+
+if (!guess) {
+    document.querySelector('.message').textcontent = 'No number!';
+}
+});
+*/
+/*
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+      document.querySelector('.message').textContent = 'No number!';
+
+} else if (guess === secretNumber) {
+
+}
+
+});  */
+/*
+const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+*/
+
+/*
+Array.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+*/
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
